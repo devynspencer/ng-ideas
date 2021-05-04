16244960dddc25cd09a22ffd0f84f0746a75f9dd
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-04 14:27:29 -0700

    Move ngrx logic into new app-store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 import { AuthService } from '@app/services/auth.service';
 import { ApiService } from '@app/services/api.service';
+import { AppStoreModule } from '@app/store/app-store.module';
 
 @NgModule({
   declarations: [
@@ -19,8 +18,7 @@ import { ApiService } from '@app/services/api.service';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument()
+    AppStoreModule,
   ],
   providers: [
     AuthService,
---
+++ src/app/store/app-store.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
+  ],
+  declarations: []
+})
+export class AppStoreModule { }
