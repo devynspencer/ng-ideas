e8e27cb937ce18f674c2d1cddae25090d96ee4fe
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-04 19:24:04 -0700

    Create reducers map and load into the store

--- src/app/store/app-store.module.ts
+++ src/app/store/app-store.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducerMap, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { errorReducer, ErrorState } from '@app/store/reducers/error.reducers';
+
+export interface AppState {
+  error: ErrorState;
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+  error: errorReducer,
+};
 
 @NgModule({
   imports: [
     CommonModule,
     EffectsModule.forRoot([]),
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument(),
   ],
   declarations: []
