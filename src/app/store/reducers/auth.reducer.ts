37d39d3bc366b1488eb67d1382a05329e0240dc2
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-04 21:03:31 -0700

    Add auth reducer

---
+++ src/app/store/reducers/auth.reducer.ts
@@ -0,0 +1,32 @@
+import { User } from '@app/models/user.model';
+import { AuthAction, AuthActionTypes } from '@app/store/actions/auth.actions';
+
+export interface AuthState {
+  user: User | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+};
+
+export const authReducer: (
+  state: AuthState,
+  action: AuthAction
+) => AuthState = (state = initialState, action: AuthAction) => {
+  switch (action.type) {
+    case AuthActionTypes.LOGIN_USER:
+      return { ...state };
+
+    case AuthActionTypes.REGISTER_USER:
+      return { ...state };
+
+    case AuthActionTypes.SET_INITIAL_USER:
+      return { ...state };
+
+    case AuthActionTypes.SET_CURRENT_USER:
+      return { ...state, user: action.payload };
+
+    default:
+      return state;
+  }
+};
