5c229a903f45c5508a3989e4b99718e552f3b358
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-04 19:20:56 -0700

    Add error reducer

---
+++ src/app/store/reducers/error.reducers.ts
@@ -0,0 +1,28 @@
+import {
+  ErrorActionTypes,
+  ErrorAction,
+} from '@app/store/actions/error.actions';
+
+export interface ErrorState {
+  error: any;
+}
+
+const initialErrorState: ErrorState = {
+  error: null,
+};
+
+export const errorReducer: (
+  state: ErrorState,
+  action: ErrorAction
+) => ErrorState = (state = initialErrorState, action: ErrorAction) => {
+  switch (action.type) {
+    case ErrorActionTypes.ADD_ERROR:
+      return { ...state, error: action.payload };
+
+    case ErrorActionTypes.REMOVE_ERROR:
+      return { ...state, error: null };
+
+    default:
+      return state;
+  }
+};
