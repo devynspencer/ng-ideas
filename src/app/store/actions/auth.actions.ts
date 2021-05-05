0e673e521883bc8c6ddadd35449d0c3f5f9311a2
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-04 21:02:56 -0700

    Add auth actions

---
+++ src/app/store/actions/auth.actions.ts
@@ -0,0 +1,32 @@
+import { Action } from '@ngrx/store';
+
+import { User } from '@app/models/user.model';
+import { AuthDto } from '@app/models/auth.model';
+
+export enum AuthActionTypes {
+  LOGIN_USER = '[AUTH] Login user',
+  REGISTER_USER = '[AUTH] Register user',
+  SET_CURRENT_USER = '[AUTH] Set current user',
+  SET_INITIAL_USER = '[AUTH] Set initial user',
+}
+
+export class LoginUser implements Action {
+  readonly type = AuthActionTypes.LOGIN_USER;
+  constructor(public payload: AuthDto) {}
+}
+
+export class RegisterUser implements Action {
+  readonly type = AuthActionTypes.REGISTER_USER;
+  constructor(public payload: AuthDto) {}
+}
+
+export class SetCurrentUser implements Action {
+  readonly type = AuthActionTypes.SET_CURRENT_USER;
+  constructor(public payload: User) {}
+}
+
+export class SetInitialUser implements Action {
+  readonly type = AuthActionTypes.SET_INITIAL_USER;
+}
+
+export type AuthAction = LoginUser | RegisterUser | SetCurrentUser | SetInitialUser;
