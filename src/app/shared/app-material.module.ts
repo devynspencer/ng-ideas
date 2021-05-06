8ae25971fdc3d3dbbb512b56dee8f2447722ecb8
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-05 17:27:41 -0700

    Create share module for simplifying imports

---
+++ src/app/shared/app-material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+const materialModules = [
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+];
+
+@NgModule({
+  imports: [...materialModules],
+  exports: [...materialModules],
+})
+export class AppMaterialModule {}
