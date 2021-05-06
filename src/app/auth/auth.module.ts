d603e5c6269acd1e2181c3f062d95d3424ee0e3f
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-05 17:29:12 -0700

    Add auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { AppMaterialModule } from '@app/shared/app-material.module';
+
+@NgModule({
+  imports: [AppMaterialModule],
+})
+export class AuthModule {}
