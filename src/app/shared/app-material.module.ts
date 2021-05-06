c79abfe7892827d8417ebf8bbb08c0533a155c60
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-06 16:46:41 -0700

    Add additional angular material submodules

--- src/app/shared/app-material.module.ts
+++ src/app/shared/app-material.module.ts
@@ -7,6 +7,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 const materialModules = [
   MatButtonModule,
@@ -14,6 +18,10 @@ const materialModules = [
   MatFormFieldModule,
   MatIconModule,
   MatInputModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule,
 ];
 
 @NgModule({
