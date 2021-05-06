f7c6775434439c8dcb066b1e81e9536f44543bbf
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-05 17:30:46 -0700

    Add auth component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import { AppState } from '@app/store/app-store.module';
+import { validateNotWhitespace } from '@app/shared/utilities/validators';
+import { LoginUser, RegisterUser } from '@app/store/actions/auth.actions';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+})
+export class AuthComponent implements OnInit {
+  authForm: FormGroup;
+  hide = true;
+
+  constructor(private fb: FormBuilder, private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.authForm = this.fb.group({
+      username: this.fb.control('', [
+        Validators.required,
+        validateNotWhitespace,
+      ]),
+      password: this.fb.control('', [
+        Validators.required,
+        validateNotWhitespace,
+      ]),
+    });
+  }
+
+  login(): void {
+    const val = this.authForm.getRawValue();
+
+    this.store.dispatch(new LoginUser(val));
+  }
+
+  register(): void {
+    const val = this.authForm.getRawValue();
+
+    this.store.dispatch(new RegisterUser(val));
+  }
+}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppMaterialModule } from '@app/shared/app-material.module';
+import { AuthComponent } from '@app/auth/auth.component';
 
 @NgModule({
-  imports: [AppMaterialModule],
+  imports: [AppMaterialModule, ReactiveFormsModule],
+  declarations: [AuthComponent],
+  exports: [AuthComponent],
 })
 export class AuthModule {}
