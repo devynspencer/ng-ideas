66422efd02dbd8e915b926751631928da7479393
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-05-17 21:49:25 -0700

    Remove error toasts (temporary)
    Error toasts are a crap pile. Revisit when energy permits.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-
-import { AppState } from '@app/store/app-store.module';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { OpenSnackBar } from '@app/store/actions';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ng-ideas';
-
-  constructor(
-    private store: Store<AppState>,
-    private snackBarService: MatSnackBar
-  ) {}
-
-  ngOnInit(): void {
-    this.store
-      .select((state) => state.error)
-      .subscribe((val) => {
-        this.store.dispatch(
-          new OpenSnackBar({
-            message: val.error?.message,
-            action: `Error ${val.error?.code}`,
-          })
-        );
-      });
-  }
-
-  showError(err): void {
-    if (err) {
-      this.snackBarService.open(
-        err.error?.message.substring(0, 47),
-        `Error ${err.error?.code}`
-      );
-    }
-  }
 }
